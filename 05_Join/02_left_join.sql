49a7e124d87524239afc9f33b969b3a3865aec1d
JABALOC <jabalo1988@gmail.com>
2025-09-14 15:29:04 +0200

    curso de sql

--- 05_Join/02_left_join.sql
+++ 05_Join/02_left_join.sql
@@ -33,16 +33,3 @@ Carla	NULL
 Tomás	NULL
 */
 
--- RIGHT JOIN
--- Es lo mismo pero devuelve los datos de la tabla derecha
-SELECT users.name, dni.dni_number FROM users
-RIGHT JOIN dni
-ON users.user_id = dni.user_id;
--- Devuelve todos los DNI, aunque no tengan usuario
-/*
-name    dni_number
-Jorge	11111111
-Irene	22222222
-NULL    33333333
-
-*/
